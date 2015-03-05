af164996a7844b39def31346be7f518c074e468f
Christopher J. Volny <cvolny@gmail.com>
2015-03-05 01:26:07 -0500

    add OPENSHIFT_REPO to PATH, change Django Dep to 1.7

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
+    sys.path.append(os.environ['OPENSHIFT_REPO_DIR'])
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangosite.settings")
 
     from django.core.management import execute_from_command_line
