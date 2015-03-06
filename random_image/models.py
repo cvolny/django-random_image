bda949818f9ce1578846ea8b0abe8935efc53411
Christopher J. Volny <cvolny@gmail.com>
2015-03-05 22:15:26 -0500

    convert to cached, hotlinked images.

--- random_image/models.py
+++ random_image/models.py
@@ -1,9 +1,6 @@
-import os
-import uuid
 from random import randint
 from django.db import models
 from django.db.models import Count
-from sorl import thumbnail
 
 
 class RandomManager(models.Manager):
@@ -13,21 +10,12 @@ class RandomManager(models.Manager):
         return self.all()[random_index]
 
 
-def upload_to_filename(instance, filename):
-    path = "random"
-    fn, ext = os.path.splitext(filename)
-    ofn = str(uuid.uuid4()) + ext
-    return os.path.join(path, ofn)
-
-
 class RandomImage(models.Model):
     title = models.CharField(max_length=255)
-    height = models.IntegerField()
-    width = models.IntegerField()
-    file = thumbnail.ImageField(upload_to=upload_to_filename, height_field="height", width_field="width")
+    url = models.URLField(max_length=512)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     objects = RandomManager()
 
-    def __unicode__(self):
-        return self.title
+    def __str__(self):
+        return u'%s' % self.title
