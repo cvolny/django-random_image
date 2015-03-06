de47d737cdd44fc73c80df40f1d25037a494a39a
Christopher J. Volny <cvolny@gmail.com>
2015-03-06 01:33:10 -0500

    merging wsgi endpoints

--- djangosite/wsgi.py
+++ djangosite/wsgi.py
@@ -1,5 +1,18 @@
 import os
+import sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangosite.settings")
+from django.conf import settings
+
+if settings.ON_OPENSHIFT:
+    sys.path.append(os.environ['OPENSHIFT_REPO_DIR'])
+    virtenv = os.environ['VIRTUAL_ENV']
+    os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python3.3/site-packages')
+    virtualenv = os.path.join(virtenv, 'bin/activate')
+    # noinspection PyBroadException
+    try:
+        os.system(virtualenv)
+    except:
+        pass
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
